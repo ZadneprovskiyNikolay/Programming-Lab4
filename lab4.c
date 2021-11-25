88dbf73ff97cd2c144595ee4099c920fa9b716cc
Nikolay <zadneprovskiywork@gmail.com>
2021-11-25 07:55:43 +0300

    feat: set field.

--- lab4.c
+++ lab4.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
 
 const int MAX_FILEPATH = 260;
 
 struct CmdArgs { 
-    char filepath[260];
-    char prop_name[4];        
     char* prop_value;
     int show, set, get;
+    char prop_name[5];        
+    char filepath[MAX_FILEPATH + 1];
 };
 
 struct String {
@@ -35,7 +37,9 @@ int parse_cmdargs(int argc, char* argv[], struct CmdArgs* cmdargs) {
 
     // Filename.
     if (*(argv[1] + 2) == 'f') {  
-        strncpy(cmdargs->filepath, argv[1] + 11, MAX_FILEPATH);        
+        int filepath_len = strlen(argv[1]) - 11;        
+        strncpy(cmdargs->filepath, argv[1] + 11, MAX_FILEPATH);  
+        cmdargs->filepath[filepath_len] = '\0';
     } 
     else {
         printf("Incorrect first argument, format: --filepath=file_name.mp3\n");
@@ -57,9 +61,9 @@ int parse_cmdargs(int argc, char* argv[], struct CmdArgs* cmdargs) {
             printf("You didn't provide value to set.");
             return 0;
         }        
-        cmdargs->show = 1;
+        cmdargs->set = 1;
         strncpy(cmdargs->prop_name, argv[2] + 6, 4);        
-        int prop_value_length = strlen(argv[3]) - 8;
+        int prop_value_length = strlen(argv[3]);
         cmdargs->prop_value = (char*) malloc(prop_value_length * sizeof(char));
         strncpy(cmdargs->prop_value, argv[3] + 8, prop_value_length);        
     } 
@@ -71,16 +75,104 @@ int parse_cmdargs(int argc, char* argv[], struct CmdArgs* cmdargs) {
     return 1;
 }
 
-int bytes_to_size(char* buffer) {
-    int res = 0;
+unsigned int bytes_to_size(char dest[4]) {
+    unsigned int res = 0;
     for (int x = 0; x < 4; x++) {
-        res += buffer[x] << (7 * (3 - x));        
-    }
+        res += dest[x] << (8 * (3 - x));
+    }    
     return res;
 }
 
-void print_metadata(char* input_filename, char name_to_print[4]) { 
-    FILE* input_file = fopen(input_filename, "rb");
+unsigned int size_to_bytes(unsigned int size, char dest[4]) {    
+    for (int x = 0; x < 4; x++) {
+        dest[x] = size >> (8 * (3 - x));
+    }        
+}
+
+void size_to_bytes_tag_header(unsigned int size, char dest[4]) {
+    for (int x = 0; x < 4; x++) {
+        dest[x] = size >> (7 * (3 - x)) & 0x7F;
+    }
+}
+
+unsigned int bytes_to_size_tag_header(char* buffer) {
+    int res = 0;
+    for (int x = 0; x < 4; x++) {
+        res += buffer[x] << (7 * (3 - x));        
+    }   
+    return res;
+}
+
+void set_field(FILE* input_file, struct CmdArgs args, int field_present, int frame_size, int first_empty_frame) {
+    FILE* output_file = fopen("new_file.mp3", "wb+");
+
+    // Number of bytes to copy before new frame(-10 because I write new tag header).
+    long cpy_bytes_num;
+    if (field_present)
+        cpy_bytes_num = ftell(input_file); 
+    else
+        cpy_bytes_num = first_empty_frame;
+    cpy_bytes_num -= 10;
+
+    // Recompute tag size and write it with header.
+    char header[10];
+    fseek(input_file, 0, SEEK_SET);
+    fread(header, 10, 1, input_file); // Read first 6 bytes from header.
+    int tag_size = bytes_to_size_tag_header(header + 6);
+    if (field_present)
+        tag_size = tag_size - frame_size + strlen(args.prop_value);
+    else
+        tag_size += 10 + strlen(args.prop_value);
+    size_to_bytes_tag_header(tag_size, &header[6]);        
+    int how_many_wrote = fwrite(header, 10, 1, output_file);
+    if (how_many_wrote != 1) {
+        printf("Wrote incorrect header(%d).\n", how_many_wrote);
+        exit(EXIT_FAILURE);
+    };                
+
+    // Write all data till current frame to the new file.
+    int ch;
+    for (long x = 0; x < cpy_bytes_num; x++) {
+        ch = fgetc(input_file);
+        fputc(ch, output_file);
+    }
+
+    // Write header of the new frame.
+    strcpy(header, args.prop_name);
+    size_to_bytes(strlen(args.prop_value) + 1, &header[4]); // Plus 1 zero byte after header.    
+    header[8] = 0x00; // Set flags to 0.
+    header[9] = 0x00;
+    fwrite(header, 10, 1, output_file);    
+    fputc(0, output_file); // Write zero byte after header.
+    fwrite(args.prop_value, strlen(args.prop_value), 1, output_file);
+    
+    // Write remaining data to file.
+    if (field_present)
+        fseek(input_file, frame_size + 10, SEEK_CUR); // Put file pointer to the end of the replaced frame.
+    else
+        fseek(input_file, first_empty_frame, SEEK_SET); // Put file pointer to the end of the replaced frame.
+    while ((ch = fgetc(input_file)) != EOF)
+        fputc(ch, output_file);
+
+    // Delete original file. Rename created file.
+    fclose(input_file);
+    fclose(output_file);    
+    remove(args.filepath);
+    usleep(1000 * 10);
+    if (rename("new_file.mp3", args.filepath)) {
+        printf("Couldn't rename file, error: %s\n", strerror(errno));        
+    } 
+}
+
+void process_cmds(struct CmdArgs args) { 
+    int field_present = 0; // For set command.
+    int saved_frame_size = 0; // Size of field to edit.
+    long first_empty_frame = -1; // Bytes padding from start of file to the first empty frame.
+
+    if (args.set)
+        remove("new_file.mp3");
+
+    FILE* input_file = fopen(args.filepath, "rb");
     if (input_file == NULL) {
         printf("Couldn't open input file.\n");
         exit(EXIT_FAILURE);
@@ -93,59 +185,66 @@ void print_metadata(char* input_filename, char name_to_print[4]) {
     init_String(&buffer, 100);
     fread(header, sizeof(header), 1, input_file);
 
-    tag_size = bytes_to_size(header + 6);
+    tag_size = bytes_to_size_tag_header(header + 6);
     
     for (int frame_idx = 0, bytes_read = 0;; frame_idx++) {
         // Read frame header.
         fread(header, sizeof(header), 1, input_file);
         bytes_read += 10;
         if (bytes_read > tag_size) break;
-        int frame_size = bytes_to_size(header + 4);
+        unsigned int frame_size = bytes_to_size(header + 4);
 
-        if (frame_size == 0) continue;
+        if (frame_size == 0) {
+            if (first_empty_frame == -1) {
+                first_empty_frame = ftell(input_file) - sizeof(header);
+            }
+            fseek(input_file, 1, SEEK_CUR); // Minimum one bytes size of frame.
+            bytes_read++;
+            continue;
+        }
 
         // Read frame name.
-        char name[5];
-        name[4] = '\0';
+        char frame_name[5];
+        frame_name[4] = '\0';
         for (int x = 0; x < 4; x++) 
-            name[x] = header[x];
+            frame_name[x] = header[x];
 
         // Read frame data.
         extend_String(&buffer, frame_size);
         fread(buffer.str, frame_size, 1, input_file);
-        bytes_read += frame_size;       
+        bytes_read += frame_size;    
         if (bytes_read > tag_size) break;
 
-        // Print frame.
-        if ((name_to_print == NULL) || (name_to_print != NULL && !strcmp(name, name_to_print))) {
-            printf("%s:\n", name, buffer.str);
+        // Print frame if needed.
+        if (args.show || (args.get && !strcmp(frame_name, args.prop_name))) {
+            printf("%s:\n", frame_name, buffer.str);
             for (int x = 0; x < frame_size; x++) {
                 printf("%c", buffer.str[x]);
             }
             printf("\n\n");
+        }        
+        else if (args.set && !strcmp(frame_name, args.prop_name)) { 
+            fseek(input_file, -(10 + frame_size), SEEK_CUR); // Move file pointer to the begining of the frame.          
+            field_present = 1;
+            saved_frame_size = frame_size;
+            break;
         }
     }    
+
+    if (args.set) {
+        set_field(input_file, args, field_present, saved_frame_size, first_empty_frame);
+    }
 
     fclose(input_file);
 }
 
 int main(int argc, char* argv[]) {    
-    // Parse arguments.
-    struct CmdArgs cmdargs = { NULL, NULL, NULL, NULL, 0, 0 };
+    struct CmdArgs cmdargs = { NULL, 0, 0, 0, { 0, 0, 0, 0, '\0' } };
     if (!parse_cmdargs(argc, argv, &cmdargs)) {
         exit(EXIT_FAILURE);
     }
 
-    if (cmdargs.show || cmdargs.get) {
-        FILE* input_file = fopen(cmdargs.filepath, "rb");
-
-        if (cmdargs.show) { // Show.
-            print_metadata(cmdargs.filepath, NULL);
-        }
-        else { // Get.
-            print_metadata(cmdargs.filepath, cmdargs.prop_name);
-        }
-    }
+    process_cmds(cmdargs);
 
     return 0;
 }
